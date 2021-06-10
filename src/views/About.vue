39e6ca541f2dda93c33a5efd68aa03056a0ab33c
kunikoyoshida <x2rainbow@gmail.com>
2021-06-10 22:17:32 +0900

    HomeWord4-Q2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="transition()">アコーディオン</button>
+    <p>アコーディオンの中身が入ります</p>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    transition() {
+      this.$router.push({ name: "Home" });
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: block;
+}
+.about button,
+.about p {
+  width: 500px;
+  height: 80px;
+  border: solid 1px;
+  border-radius: 0;
+}
+.about button {
+  border-bottom: 0;
+  text-align: left;
+  font-size: 18px;
+}
+.about p {
+  border-top: 0;
+  align-items: center;
+  font-size: 16px;
+  margin-top: 0;
+  padding: 25px;
+  box-sizing: border-box;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="transition()">アコーディオン</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    transition() {
+      this.$router.push({ name: "About"});
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.home button {
+  width: 500px;
+  height: 80px;
+  font-size: 18px;
+  text-align: left;
+}
+</style>
